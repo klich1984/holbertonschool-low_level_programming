8c48988d6d7fff3bd9d4aaa835d5807fc4bbe1e3
klich1984 <2239@holbertonschool.com>
2021-01-15 16:27:30 -0500

    01 | code update | output good code running add free table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->size = size;
 	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
-
+	}
 	return (table);
 }
