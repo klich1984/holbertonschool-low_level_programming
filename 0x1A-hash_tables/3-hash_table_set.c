6f7f1cd869c93be342918e101e8fea2914edb3d1
klich1984 <2239@holbertonschool.com>
2021-01-15 18:23:43 -0500

    00 | add file | test implemt hash_table_set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_set - function that adds an element to the hash table
+* @key: pointer a string what containe the key
+* @value: pointer a string what containe the value
+* Return: 1 if it succeeded, 0 otherwise
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	int result = 0;
+	(void) ht;
+	(void) result;
+
+	if (key == NULL || value == NULL)
+	{
+		printf("Key o value this empty");
+		/*free mallocs*/
+		return 0;
+	}
+
+	index = hash_djb2((unsigned char *)key);  /*(unsigned char *) cast*/
+	printf("el index = %ld\n", index);
+	return 1;
+}
