6e360c21467956576c62e4bfde2a445668143138
klich1984 <2239@holbertonschool.com>
2021-01-15 21:25:53 -0500

    02 | code update | implementation function add:elemnt_hash_table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,5 +42,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+int add_elemnt_hash_table(hash_table_t *ht, unsigned long int idx,
+						const char *key, const char *value);
 
 #endif
