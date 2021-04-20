78bad75a71364bbc6a4496555b50b3a7e2962631
klich1984 <2239@holbertonschool.com>
2021-04-20 14:00:03 -0500

    00 | add file | header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
